e00dc673df69add1013a2f4aa981126d30164ebf
Jitgitbit <rowowolf@gmail.com>
2020-05-17 11:05:20 +0200

    Manipulating Request Objects

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -2,7 +2,9 @@ import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http'
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler){
-    console.log("Request is on it's way");                   // even more granular control!
-    return next.handle(req); 
+    console.log("Request is on it's way");                   // even more granular control! even f.e. for authentication!
+    console.log(`----> For which request did the interceptor run? (req.url):`,req.url);
+    const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')})
+    return next.handle(modifiedRequest); 
   };
 }
